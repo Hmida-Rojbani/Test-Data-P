da8b31cea297a1a13dcba296a9f5f5df55b71c3e
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-11-05 19:25:46 +0100

    Update getById

--- src/main/java/de/tekup/project/rest/controllers/PersonRest.java
+++ src/main/java/de/tekup/project/rest/controllers/PersonRest.java
@@ -1,9 +1,14 @@
 package de.tekup.project.rest.controllers;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +37,16 @@ public class PersonRest {
 	@PostMapping
 	public PersonEntity createPerson(@RequestBody PersonEntity personRequest) {
 		return service.savePerson(personRequest);
+	}
+	
+	@GetMapping("/{id}")
+	public PersonEntity getById(@PathVariable("id") long id){
+		return service.getPersonById(id);
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+		return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
 	}
 	
 
--- src/main/java/de/tekup/project/services/PersonServiceImpl.java
+++ src/main/java/de/tekup/project/services/PersonServiceImpl.java
@@ -1,6 +1,8 @@
 package de.tekup.project.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,23 +12,38 @@ import de.tekup.project.repositories.PersonRepository;
 
 @Service
 public class PersonServiceImpl {
-	
+
 	private PersonRepository repository;
-	
+
 	@Autowired
 	public PersonServiceImpl(PersonRepository repository) {
 		super();
 		this.repository = repository;
 	}
-	
+
 	// Retrieve all instance in database
-	public List<PersonEntity> getAllEntities(){
+	public List<PersonEntity> getAllEntities() {
 		return repository.findAll();
 	}
-	
-	// Save a request into Database 
+
+	// Save a request into Database
 	public PersonEntity savePerson(PersonEntity person) {
 		return repository.save(person);
+	}
+
+	// Get Person By Id
+	public PersonEntity getPersonById(long id) {
+		
+		Optional<PersonEntity> opt = repository.findById(id);
+		PersonEntity person = null;
+		/*if(opt.isPresent())
+			person = opt.get();
+		else
+			throw new NoSuchElementException("Person with this id is "
+					+ "not found");*/
+		person = opt.orElseThrow(()->new NoSuchElementException("Person "
+				+ "with this id is not found"));
+		return person;
 	}
 
 }
